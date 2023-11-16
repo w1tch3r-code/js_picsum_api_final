32a1053169ced7de67cf9fdcf954395c986b45f9
w1tch3r-code <rainerbrain@gmail.com>
2023-11-16 15:41:25 +0100

    Buttons und window.open() hinzugefügt

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ console.log("%c JS-Vertiefung – API Picsum", "color: tomato");
 // Aufgabenstellung
 
 // Für dieses Projekt verwenden wir die API von Picsum, um eine Galerie zu erstellen.
-// Schau dir das Vorschauvideo (s. nächste Seite) genau an. Wenn man auf den Button klickt, öffnet sich ein neues Fenster. 
+// Schau dir das Vorschauvideo (s. nächste Seite) genau an. Wenn man auf den Button klickt, öffnet sich ein neues Fenster.
 
 // https://picsum.photos/ https://picsum.photos/v2/list
 
@@ -20,10 +20,11 @@ console.log("%c JS-Vertiefung – API Picsum", "color: tomato");
 // appendChild()
 // window.open() könnte für dich nützlich sein
 
+// Mit buttons und window.open()
 
 const fetchPicsum = () => {
-	const wrapper = document.querySelector('.wrapper');
-
+	const wrapper = document.querySelector(".wrapper");
+	//https://picsum.photos/v2/list?page&limit=100
 	fetch("https://picsum.photos/v2/list")
 		.then((response) => {
 			if (response.ok === false) {
@@ -37,19 +38,19 @@ const fetchPicsum = () => {
 				const newFigure = document.createElement("figure");
 				const newImage = document.createElement("img");
 				const newFigcaption = document.createElement("figcaption");
-				const newATag = document.createElement("a");
+				const newButton = document.createElement("button");
 
-				newImage.setAttribute('src', `${elem.download_url}`);
-				newImage.setAttribute('alt', `${elem.author}`);
-				newFigure.appendChild(newImage)
+				newImage.setAttribute("src", `${elem.download_url}`);
+				newImage.setAttribute("alt", `${elem.author}`);
 
 				newFigcaption.textContent = elem.author;
-				newFigure.appendChild(newFigcaption)
 
-				newATag.setAttribute('href', `${elem.url}`);
-				newATag.setAttribute('target', '_blank');
-				newATag.textContent = `See more`;
-				newFigure.appendChild(newATag)
+				newButton.addEventListener("click", () => {
+					window.open(elem.url, "_blank");
+				});
+
+				newButton.textContent = `See more`;
+				newFigure.append(newImage, newFigcaption, newButton);
 
 				wrapper.appendChild(newFigure);
 			});
@@ -58,3 +59,39 @@ const fetchPicsum = () => {
 };
 
 fetchPicsum();
+
+// Mit a-Tags
+
+// const fetchPicsum = () => {
+// 	const wrapper = document.querySelector('.wrapper');
+// 	fetch("https://picsum.photos/v2/list")
+// 		.then((response) => {
+// 			if (response.ok === false) {
+// 				throw new Error("Hier ist etwas schief gelaufen");
+// 			}
+// 			return response.json();
+// 		})
+// 		.then((data) => {
+// 			data.forEach((elem) => {
+// 				const newFigure = document.createElement("figure");
+// 				const newImage = document.createElement("img");
+// 				const newFigcaption = document.createElement("figcaption");
+// 				const newATag = document.createElement("a");
+
+// 				newImage.setAttribute('src', `${elem.download_url}`);
+// 				newImage.setAttribute('alt', `${elem.author}`);
+
+// 				newFigcaption.textContent = elem.author;
+
+// 				newATag.setAttribute('href', `${elem.url}`);
+// 				newATag.setAttribute('target', '_blank');
+// 				newATag.textContent = `See more`;
+// 				newFigure.append(newImage, newFigcaption, newATag)
+
+// 				wrapper.appendChild(newFigure);
+// 			});
+// 		})
+// 		.catch((error) => console.log(error));
+// };
+
+// fetchPicsum();
